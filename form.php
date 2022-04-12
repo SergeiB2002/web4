13bb2ff9cc4c3a0efad8a3ce352436b01f02b3da
Sergei Brezitskii <90458734+SergeiB2002@users.noreply.github.com>
2022-04-12 14:31:31 +0300

    Update form.php

--- form.php
+++ form.php
@@ -139,11 +139,21 @@ if (!empty($messages)) {
             var ddlYears = document.getElementById("ddlYears");
             var currentYear = (new Date()).getFullYear();
             for (var i = 1950; i <= currentYear; i++) {
-                var option = document.createElement("OPTION");
-                option.innerHTML = i;
-                option.value = i;
-                ddlYears.appendChild(option);
-		    if ($_COOKIE['year_value']
+		    if ($value['year']==i){
+			    
+		    var option = document.createElement("OPTION");
+                    option.innerHTML = i;
+                    option.value = i;
+			      option.selected=true;
+                    ddlYears.appendChild(option);
+			  
+		    }
+		    else{
+			   var option = document.createElement("OPTION");
+                           option.innerHTML = i;
+                           option.value = i;
+                           ddlYears.appendChild(option);
+		    }
             }
         };
     </script>
