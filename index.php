4bb426650a2c399ad491acc779f6f8b1fb43d553
Sergei Brezitskii <90458734+SergeiB2002@users.noreply.github.com>
2022-04-12 13:58:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
   // Выдаем сообщения об ошибках.
   if ($errors['field-name']) {
-    setcookie('fio_error', '', 100000);
+    setcookie('name_error', '', 100000);
     $messages[] = '<div class="error">Заполните имя.</div>';
   }
   if ($errors['field-email']) {
@@ -57,32 +57,106 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $messages[] = '<div class="error">Вы должны болеть за Red Bull Racing.</div>';
   }
   
-
   // Складываем предыдущие значения полей в массив, если есть.
   $values = array();
   $values['field-name'] = empty($_COOKIE['name_value']) ? '' : $_COOKIE['name_value'];
   $values['field-email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
+  $values['year'] = empty($_COOKIE['year_value']) ? 0 : $_COOKIE['year_value'];
+  $values['radio-pol'] = empty($_COOKIE['pol_value']) ? '' : $_COOKIE['pol_value'];
+  $values['radio-limb'] = empty($_COOKIE['limb_value']) ? '' : $_COOKIE['limb_value'];
+  $values['immortal'] = empty($_COOKIE['immortal_value']) ? 0 : $_COOKIE['immortal_value'];
+  $values['noclip'] = empty($_COOKIE['noclip_value']) ? 0 : $_COOKIE['noclip_value'];
+  $values['power'] = empty($_COOKIE['power_value']) ? 0 : $_COOKIE['power_value'];
+  $values['telepat'] = empty($_COOKIE['telepat_value']) ? 0 : $_COOKIE['telepat_value'];
   $values['field-bio'] = empty($_COOKIE['bio_value']) ? '' : $_COOKIE['bio_value'];
-  // TODO: аналогично все поля.
+  $values['checkbox'] = empty($_COOKIE['check_value']) ? 0 : $_COOKIE['check_value'];
 
   // Включаем содержимое файла form.php.
   // В нем будут доступны переменные $messages, $errors и $values для вывода 
   // сообщений, полей с ранее заполненными данными и признаками ошибок.
   include('form.php');
 }
-// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
 else {
   // Проверяем ошибки.
   $errors = FALSE;
-  if (empty($_POST['fio'])) {
-    // Выдаем куку на день с флажком об ошибке в поле fio.
-    setcookie('fio_error', '1', time() + 24 * 60 * 60);
+  if (empty($_POST['field-name'])) {
+    setcookie('name_error', '1', time() + 24 * 60 * 60);
+    setcookie('name_value', '', 100000);
     $errors = TRUE;
   }
   else {
-    // Сохраняем ранее введенное в форму значение на месяц.
-    setcookie('fio_value', $_POST['fio'], time() + 30 * 24 * 60 * 60);
+    // Сохраняем ранее введенное в форму значение на год.
+    setcookie('name_value', $_POST['field-name'], time() + 12 * 30 * 24 * 60 * 60);
+    setcookie('name_error', '', 100000);
   }
+  
+  if (empty($_POST['field-email'])) {
+    setcookie('email_error', '1', time() + 24 * 60 * 60);
+    setcookie('email_value', '', 100000);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('email_value', $_POST['field-email'], time() + 12 * 30 * 24 * 60 * 60);
+    setcookie('email_error', '', 100000);
+  }
+  
+  if (empty($_POST['year'])) {
+    setcookie('year_error', '1', time() + 24 * 60 * 60);
+    setcookie('year_value', '', 100000);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('year_value', $_POST['year'], time() + 12 * 30 * 24 * 60 * 60);
+    setcookie('year_error', '', 100000);
+  }
+  
+  if (!isset($_POST['radio-pol'])) {
+  setcookie('pol_error', '1', time() + 24 * 60 * 60);
+  setcookie('pol_value', '', 100000);
+  $errors = TRUE;
+}
+else {
+  setcookie('pol_value', $_POST['radio-pol'], time() + 12*30 * 24 * 60 * 60);
+  setcookie('pol_error','',100000);
+}
+  
+  if (!isset($_POST['radio-limb'])) {
+  setcookie('limb_error', '1', time() + 24 * 60 * 60);
+  setcookie('limb_value', '', 100000);
+  $errors = TRUE;
+}
+else {
+  setcookie('limb_value', $_POST['radio-limb'], time() + 12*30 * 24 * 60 * 60);
+  setcookie('limb_error','',100000);
+}
+  
+  if (!isset($_POST['field-super'])) {
+  setcookie('super_error', '1', time() + 24 * 60 * 60);
+  setcookie('immortal_value', '', 100000);
+  setcookie('noclip_value', '', 100000);
+  setcookie('power_value', '', 100000);
+  setcookie('telepat_value', '', 100000);
+  $errors = TRUE;
+}
+else {
+  $powrs=$_POST['field-super'];
+  $apw=array(
+    "immortal_value"=>0,
+    "noclip_value"=>0,
+    "power_value"=>0
+    "telepat_value"=>0
+  );
+  foreach($powrs as $pwer){
+    if($pwer=='бессмертие'){setcookie('immortal_value', 1, time() + 12*30 * 24 * 60 * 60); $apw['immortal_value']=1;} 
+    if($pwer=='прохождение сквозь стены'){setcookie('ghost_value', 1, time() + 12*30 * 24 * 60 * 60);$apw['ghost_value']=1;} 
+    if($pwer=='левитация'){setcookie('levitation_value', 1, time() + 12*30 * 24 * 60 * 60);$apw['levitation_value']=1;} 
+  }
+  foreach($a as $c=>$val){
+    if($val==0){
+      setcookie($c,'',100000);
+    }
+  }
+}
 
 // *************
 // TODO: тут необходимо проверить правильность заполнения всех остальных полей.
